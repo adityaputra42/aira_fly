3d2ecf3cb8484529a3351caed0d25cfa97e586d6
adityaputra42 <aditiyaputra42@gmail.com>
2026-09-21 23:00:31 +0700

    updateenv

--- lib/core/constants/environment.dart
+++ lib/core/constants/environment.dart
@@ -2,25 +2,25 @@ import 'package:envied/envied.dart';
 
 part 'environment.g.dart';
 
-@Envied(path: '.env')
+@Envied(path: '.env', obfuscate: true)
 abstract class Env {
   @EnviedField(varName: 'APP_ENV')
-  static const String appEnv = _Env.appEnv;
+  static String appEnv = _Env.appEnv;
 
   @EnviedField(varName: 'DEV_URL')
-  static const String devUrl = _Env.devUrl;
+  static String devUrl = _Env.devUrl;
 
   @EnviedField(varName: 'STAGING_URL')
-  static const String stagingUrl = _Env.stagingUrl;
+  static String stagingUrl = _Env.stagingUrl;
 
   @EnviedField(varName: 'PRODUCTION_URL')
-  static const String productionUrl = _Env.productionUrl;
+  static String productionUrl = _Env.productionUrl;
 
   @EnviedField(varName: 'APP_USERNAME_LOGIN')
-  static const String appUsernameLogin = _Env.appUsernameLogin;
+  static String appUsernameLogin = _Env.appUsernameLogin;
 
   @EnviedField(varName: 'APP_PASSWORD_LOGIN')
-  static const String appPasswordLogin = _Env.appPasswordLogin;
+  static String appPasswordLogin = _Env.appPasswordLogin;
 
   static String getApiBaseUrl() {
     switch (appEnv) {
--- lib/core/constants/environment.g.dart
+++ lib/core/constants/environment.g.dart
@@ -10,15 +10,349 @@ part of 'environment.dart';
 // ignore_for_file: type=lint
 // generated_from: .env
 final class _Env {
-  static const String appEnv = 'dev';
+  static const List<int> _enviedkeyappEnv = <int>[
+    2955839697,
+    871293477,
+    1905959464,
+  ];
 
-  static const String devUrl = 'https://dev.agata27.site/api/v1/';
+  static const List<int> _envieddataappEnv = <int>[
+    2955839669,
+    871293504,
+    1905959518,
+  ];
 
-  static const String stagingUrl = 'https://dev.agata27.site/api/v1/';
+  static final String appEnv = String.fromCharCodes(
+    List<int>.generate(
+      _envieddataappEnv.length,
+      (int i) => i,
+      growable: false,
+    ).map((int i) => _envieddataappEnv[i] ^ _enviedkeyappEnv[i]),
+  );
 
-  static const String productionUrl = 'https://dev.agata27.site/api/v1/';
+  static const List<int> _enviedkeydevUrl = <int>[
+    1667061122,
+    4229909543,
+    4071309102,
+    3369419470,
+    3863784829,
+    2332807771,
+    3592727410,
+    2362709755,
+    920860897,
+    3895851273,
+    2173022908,
+    542170863,
+    3610282098,
+    4117754518,
+    3681516359,
+    2630318636,
+    2793790436,
+    2120203207,
+    368184308,
+    690232385,
+    1106970799,
+    625006393,
+    3791255139,
+    4057431178,
+    1391159234,
+    301155212,
+    931366344,
+    2060215653,
+    3176501199,
+    2139426062,
+    1152340831,
+    2530267560,
+  ];
 
-  static const String appUsernameLogin = 'customer@pss.local';
+  static const List<int> _envieddatadevUrl = <int>[
+    1667061226,
+    4229909587,
+    4071309146,
+    3369419454,
+    3863784718,
+    2332807777,
+    3592727389,
+    2362709716,
+    920860805,
+    3895851372,
+    2173022922,
+    542170817,
+    3610282003,
+    4117754609,
+    3681516326,
+    2630318680,
+    2793790341,
+    2120203253,
+    368184259,
+    690232431,
+    1106970844,
+    625006416,
+    3791255063,
+    4057431279,
+    1391159277,
+    301155309,
+    931366328,
+    2060215564,
+    3176501216,
+    2139426168,
+    1152340846,
+    2530267527,
+  ];
 
-  static const String appPasswordLogin = 'Cust0mer!123';
+  static final String devUrl = String.fromCharCodes(
+    List<int>.generate(
+      _envieddatadevUrl.length,
+      (int i) => i,
+      growable: false,
+    ).map((int i) => _envieddatadevUrl[i] ^ _enviedkeydevUrl[i]),
+  );
+
+  static const List<int> _enviedkeystagingUrl = <int>[
+    3132579800,
+    3964496276,
+    2526222912,
+    1419581013,
+    746438148,
+    3256581606,
+    3417982786,
+    3183130739,
+    36507238,
+    80025583,
+    3228882141,
+    462632942,
+    1942482584,
+    19031723,
+    757741592,
+    1027076168,
+    2052818520,
+    1836942269,
+    1402616061,
+    2193508447,
+    2258974948,
+    878621969,
+    1205991654,
+    3414567595,
+    1364693329,
+    373011947,
+    4062911122,
+    2525579521,
+    1058562035,
+    2246420726,
+    1453585544,
+    1816333813,
+  ];
+
+  static const List<int> _envieddatastagingUrl = <int>[
+    3132579760,
+    3964496352,
+    2526222900,
+    1419580965,
+    746438263,
+    3256581596,
+    3417982829,
+    3183130716,
+    36507138,
+    80025482,
+    3228882091,
+    462632896,
+    1942482681,
+    19031756,
+    757741689,
+    1027076156,
+    2052818489,
+    1836942223,
+    1402616010,
+    2193508465,
+    2258974871,
+    878622072,
+    1205991570,
+    3414567630,
+    1364693374,
+    373011850,
+    4062911202,
+    2525579624,
+    1058562012,
+    2246420608,
+    1453585593,
+    1816333786,
+  ];
+
+  static final String stagingUrl = String.fromCharCodes(
+    List<int>.generate(
+      _envieddatastagingUrl.length,
+      (int i) => i,
+      growable: false,
+    ).map((int i) => _envieddatastagingUrl[i] ^ _enviedkeystagingUrl[i]),
+  );
+
+  static const List<int> _enviedkeyproductionUrl = <int>[
+    3170466313,
+    363558922,
+    2346748719,
+    39199233,
+    2854366641,
+    4258987063,
+    1512226110,
+    3832339710,
+    1491357334,
+    3529932204,
+    2522865998,
+    3343301771,
+    2817810829,
+    428342112,
+    3152268820,
+    3122158474,
+    1512590511,
+    2083378963,
+    2282236748,
+    287244201,
+    2237546564,
+    3588821130,
+    3573848315,
+    1100444164,
+    1475738641,
+    35547809,
+    1264968561,
+    522151087,
+    98210318,
+    1866380747,
+    2355127265,
+    2968218990,
+  ];
+
+  static const List<int> _envieddataproductionUrl = <int>[
+    3170466401,
+    363559038,
+    2346748763,
+    39199345,
+    2854366658,
+    4258987021,
+    1512226065,
+    3832339665,
+    1491357426,
+    3529932233,
+    2522865976,
+    3343301797,
+    2817810924,
+    428342023,
+    3152268917,
+    3122158590,
+    1512590542,
+    2083378977,
+    2282236795,
+    287244167,
+    2237546551,
+    3588821219,
+    3573848207,
+    1100444257,
+    1475738686,
+    35547840,
+    1264968449,
+    522151110,
+    98210337,
+    1866380733,
+    2355127248,
+    2968218945,
+  ];
+
+  static final String productionUrl = String.fromCharCodes(
+    List<int>.generate(
+      _envieddataproductionUrl.length,
+      (int i) => i,
+      growable: false,
+    ).map((int i) => _envieddataproductionUrl[i] ^ _enviedkeyproductionUrl[i]),
+  );
+
+  static const List<int> _enviedkeyappUsernameLogin = <int>[
+    77556861,
+    3950127929,
+    3816417302,
+    501392093,
+    1811322140,
+    759873291,
+    1710056776,
+    4120046396,
+    2468521607,
+    2902124425,
+    1506109221,
+    1107799141,
+    667815071,
+    4155960820,
+    2567844321,
+    3074633098,
+    1912082012,
+    2281134504,
+  ];
+
+  static const List<int> _envieddataappUsernameLogin = <int>[
+    77556766,
+    3950127948,
+    3816417381,
+    501392041,
+    1811322227,
+    759873382,
+    1710056749,
+    4120046414,
+    2468521671,
+    2902124537,
+    1506109270,
+    1107799062,
+    667815089,
+    4155960728,
+    2567844238,
+    3074633193,
+    1912081981,
+    2281134532,
+  ];
+
+  static final String appUsernameLogin = String.fromCharCodes(
+    List<int>.generate(
+      _envieddataappUsernameLogin.length,
+      (int i) => i,
+      growable: false,
+    ).map(
+      (int i) => _envieddataappUsernameLogin[i] ^ _enviedkeyappUsernameLogin[i],
+    ),
+  );
+
+  static const List<int> _enviedkeyappPasswordLogin = <int>[
+    2239427903,
+    1618344167,
+    449699968,
+    2651732809,
+    283078962,
+    1841584374,
+    173798860,
+    924010580,
+    2240611584,
+    165300375,
+    2699594349,
+    3718408617,
+  ];
+
+  static const List<int> _envieddataappPasswordLogin = <int>[
+    2239427964,
+    1618344082,
+    449700083,
+    2651732797,
+    283078914,
+    1841584283,
+    173798825,
+    924010534,
+    2240611617,
+    165300390,
+    2699594335,
+    3718408602,
+  ];
+
+  static final String appPasswordLogin = String.fromCharCodes(
+    List<int>.generate(
+      _envieddataappPasswordLogin.length,
+      (int i) => i,
+      growable: false,
+    ).map(
+      (int i) => _envieddataappPasswordLogin[i] ^ _enviedkeyappPasswordLogin[i],
+    ),
+  );
 }
